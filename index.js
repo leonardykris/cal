870548b4c19d12c0a8cb3b00f9f1b8a4397e6a54
Leonardy Kristianto <alendory@gmail.com>
2017-05-06 14:44:49 +0700

    removed heroku boilerplate texts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,7 +12,32 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/db');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/times', function(request, response) {
+  var result = '';
+  var times = process.env.TIMES || 5;
+  for (i=0; i < times; i++) {
+    result += i + ' ';
+  }
+  response.send(result);
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
