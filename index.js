c3fe066c064e22780cfd3e2c0bee3908f3be7528
Leonardy Kristianto <alendory@gmail.com>
2017-05-06 21:01:09 +0700

    added some standard route, commented out old boilerplate codes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,36 +7,44 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
-  response.render('pages/db');
+  response.render('pages/dashboard');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/program', function(request, response) {
+  response.render('pages/program', {params: "content from request"});
 });
 
-app.get('/times', function(request, response) {
-  var result = '';
-  var times = process.env.TIMES || 5;
-  for (i=0; i < times; i++) {
-    result += i + ' ';
-  }
-  response.send(result);
+app.get('/login', function(request, response) {
+  response.render('components/login');
 });
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
+
+// app.get('/times', function(request, response) {
+//   var result = '';
+//   var times = process.env.TIMES || 5;
+//   for (i=0; i < times; i++) {
+//     result += i + ' ';
+//   }
+//   response.send(result);
+// });
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
